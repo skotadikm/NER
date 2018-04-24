43fd40cb3a72f8be1cefe3ad8b319160c17a6c4b
skotadikm <tin.kming@gmail.com>
2018-04-25 04:12:55 +0700

    add dic

--- extract_features.py
+++ extract_features.py
@@ -22,18 +22,9 @@ vocab_count = 0
 
 #original dictionary
 dic_list = [["common"],["loc_name"],["loc_clue"],["org_name"],["org_clue"],["per_clue"],["per_first"],["per_last"]]
-common = []
-loc_name = []
-loc_clue = []
-org_name = []
-org_clue = []
-per_clue = []
-per_first = []
-per_last = []
 
 def create_dic(i):
     #แก้ไขpath dic
-    #global dic_list
     filenames = dic_list[i][0]
     txt_file = open(filenames+".txt","r",encoding="utf-8-sig")
     text = txt_file.read()
@@ -41,7 +32,6 @@ def create_dic(i):
     raw = re.split(r'\n', text)
     for comp in raw:
         dic_list[i].append(comp)
-        #eval("%s.append(comp)" %dic_list[i])
 
 def read_txt(filenames):
     global corpus_text
@@ -164,17 +154,349 @@ def special_char(word):
 def English(word):
     return word.isalpha()
 
+def dic_check():
+    temp = ["ool","ool","ool","ool","ool","ool","ool","ool","ool","ool","ool"]
+    for i in range(vocab_count):
+        temp[5] = vocab[i][0]
+        for j in range(6):
+            if(i-j >= 0):
+                temp[5-j] = vocab[i-j][0]
+            if(i+j < vocab_count):
+                temp[5+j] = vocab[i+j][0]
+        ########ได้list w ที่มาเช็ค
+        init = 19
+        for k in range(dic_list):
+            new_temp = temp    
+            new_temp = match_10(dic_list[k],new_temp)
+            for l in range(len(temp)):
+                vocab[i][init+l] = new_temp[l]
+            init += 11
+
+def match_10(dic, temp):
+    word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4] + temp[i+5] + temp[i+6] + temp[i+7] + temp[i+8] + temp[i+9] + temp[i+10]
+    result = dic_compare(dic, word)
+    if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_cont"
+                temp[i+5] = dic[0] + "_cont"
+                temp[i+6] = dic[0] + "_cont"
+                temp[i+7] = dic[0] + "_cont"
+                temp[i+8] = dic[0] + "_cont"
+                temp[i+9] = dic[0] + "_cont"
+                temp[i+10] = dic[0] + "_end"
+    else:
+        temp = match_10(dic, temp)
+    return temp
+
+def match_10(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+9 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4] + temp[i+5] + temp[i+6] + temp[i+7] + temp[i+8] + temp[i+9]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_cont"
+                temp[i+5] = dic[0] + "_cont"
+                temp[i+6] = dic[0] + "_cont"
+                temp[i+7] = dic[0] + "_cont"
+                temp[i+8] = dic[0] + "_cont"
+                temp[i+9] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_9(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_9(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_9(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+8 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4] + temp[i+5] + temp[i+6] + temp[i+7] + temp[i+8]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_cont"
+                temp[i+5] = dic[0] + "_cont"
+                temp[i+6] = dic[0] + "_cont"
+                temp[i+7] = dic[0] + "_cont"
+                temp[i+8] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_8(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_8(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_8(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+7 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4] + temp[i+5] + temp[i+6] + temp[i+7]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_cont"
+                temp[i+5] = dic[0] + "_cont"
+                temp[i+6] = dic[0] + "_cont"
+                temp[i+7] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_7(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_7(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_7(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+6 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4] + temp[i+5] + temp[i+6]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_cont"
+                temp[i+5] = dic[0] + "_cont"
+                temp[i+6] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_6(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_6(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_6(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+5 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4] + temp[i+5]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_cont"
+                temp[i+5] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_5(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_5(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_5(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+4 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3] + temp[i+4]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_cont"
+                temp[i+4] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_4(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_4(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_4(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+3 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2] + temp[i+3]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_cont"
+                temp[i+3] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_3(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_3(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_3(dic, temp):
+    new_temp = []
+    pointer = 12
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+2 < len(temp)):
+            word = temp[i] + temp[i+1] + temp[i+2]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_cont"
+                temp[i+2] = dic[0] + "_end"
+                if(new_temp != []):
+                    new_temp = match_2(dic, new_temp)
+                    for j in range(len(new_temp)):
+                        temp[pointer+j] = new_temp[j]
+                new_temp = []
+                pointer = 12
+            else:
+                if(pointer > i):
+                    pointer = i
+                new_temp.append(temp[i])
+        elif(temp[i] == "False"):
+            if(pointer > i):
+                    pointer = i
+            new_temp.append(temp[i])
+    if(new_temp != []):
+        new_temp = match_2(dic, new_temp)
+                for i in range(len(new_temp)):
+                    temp[pointer+i] = new_temp[i]
+    return temp
+
+def match_2(dic, temp):
+    for i in range(len(temp)):
+        if(temp[i] == "False" and i+1 < len(temp)):
+            word = temp[i] + temp[i+1]
+            result = dic_compare(dic, word)
+            if(result == dic[0]):
+                temp[i] = dic[0] + "_start"
+                temp[i+1] = dic[0] + "_end"
+            else:
+                temp[i] = dic_compare(dic,temp[0])
+        elif(temp[i] == "False"):
+            temp[i] = dic_compare(dic,temp[0])
+    return temp
+
+def dic_compare(dic, test):
+    for word in dic[1:]:
+        if(test = word):
+            test = dic[0]
+            return test
+
 if __name__ == '__main__':
     read_txt(filename)
     for i in range(len(dic_list)):
         create_dic(i)
     create_vocab(corpus_text)
     vocab_check()
-    print(dic_list[2])
-    #dic_check()
+    dic_check()
     print(corpus_text)
-    #for i in vocab:
-    #    print(i)
+    for i in vocab:
+        print(i)
     print("Total is " + str(len(vocab)) + " word")
     # god tum ja
     f = open("output.txt","w+")
