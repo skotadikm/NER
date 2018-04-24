2562793a27f32405be35a8cc6d0fb5371bc6b1b8
skotadikm <tin.kming@gmail.com>
2018-04-25 04:36:17 +0700

    add dic

--- extract_features.py
+++ extract_features.py
@@ -163,7 +163,7 @@ def dic_check():
                 temp[5-j] = vocab[i-j][0]
             if(i+j < vocab_count):
                 temp[5+j] = vocab[i+j][0]
-            if(i+j > vocab_count):
+            if(i+j >= vocab_count):
                 temp[5+j] = "ool"
         ########ได้list w ที่มาเช็ค
         print(temp)
