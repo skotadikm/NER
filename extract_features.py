dc20dcbd5c2975c895a74137059b7ddb3d05b769
skotadikm <tin.kming@gmail.com>
2018-04-28 06:09:51 +0700

    add dic

--- extract_features.py
+++ extract_features.py
@@ -199,7 +199,7 @@ def dic_check(dic,index):
     for i in range(vocab_count):
         checklist = ["False",0]
         for j in range(vocab_count):
-            if(i+j > vocab_count):
+            if(i+j > vocab_count-1):
                 break
             tmp += vocab[i+j][0]
             print(tmp)
