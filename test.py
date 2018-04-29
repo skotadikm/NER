18f65e7e7a90214d58a64240c5782e5892727aff
skotadikm <tin.kming@gmail.com>
2018-04-30 06:16:30 +0700

    measure

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ import sys
 
 model = sys.argv[1]
 test_data = sys.argv[2]
+confusion_matrix = []
 
 def crf_test():
     cmd = os.popen(r'crf_test -m ' + model + ' ' + test_data)
@@ -17,18 +18,28 @@ def crf_test():
 
 def create_confusion_matrix(test):
     tmp1 = test.splitlines()
-    tag = []
     for line in tmp1[:len(tmp1)-1]:
-        tmp2 = []
-        i = re.split(r'\t', line)
-        pointer = len(i)
-        tmp2.append(i[pointer-2])
-        tmp2.append(i[pointer-1])
-        tag.append(tmp2)
-    return tag
+                raw = re.split(r'\t', line)
+        pointer = len(raw)
+        if(len(confusion_matrix) == 0):
+            confusion_matrix.append([raw[pointer-2]])
+        else:
+            count = 0
+            for i in range(len(confusion_matrix)):
+                if(raw[pointer-2] == confusion_matrix[i][0]):
+                    count += 1
+            if(count == 0):
+                confusion_matrix.append([raw[pointer-2]])
+    for i in range(len(confusion_matrix)):
+        for j in range(len(confusion_matrix)):
+            confusion_matrix[i][j].append(0)
+
+        
 
 
 if __name__ == '__main__':
     test = crf_test()
     tmp = create_confusion_matrix(test)
-    print(tmp)
+    print(confusion_matrix)
+
+
