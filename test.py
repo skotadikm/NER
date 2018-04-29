eb32e5637144a6e1f1f46fbf9efb7a8c5d8693fd
skotadikm <tin.kming@gmail.com>
2018-04-30 05:50:15 +0700

    measure

--- test.py
+++ test.py
@@ -10,24 +10,24 @@ import sys
 model = sys.argv[1]
 test_data = sys.argv[2]
 
-def crf_test(model, test_data):
+def crf_test():
     cmd = os.popen(r'crf_test -m ' + model + ' ' + test_data)
     result = cmd.read()
     return result
 
 def create_confusion_matrix(test):
     tmp1 = test.splitlines()
-    print(tmp1)
     for line in tmp1:
         tmp2 = []
         i = re.split(r'\t', line)
         pointer = len(i)
         tmp2.append(pointer-1)
         tmp2.append(pointer)
-    return tmp2
+        tag.append(tmp2)
+    return tag
 
 
 if __name__ == '__main__':
-    test = crf_test(model, test_data)
+    test = crf_test()
     tmp = create_confusion_matrix(test)
     print(tmp)
