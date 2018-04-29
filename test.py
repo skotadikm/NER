a9d5a7a56d16923ba9304db6ee54821f4ce34396
skotadikm <tin.kming@gmail.com>
2018-04-30 05:44:04 +0700

    add dic

--- test.py
+++ test.py
@@ -9,7 +9,22 @@ import sys
 
 model = sys.argv[1]
 test_data = sys.argv[2]
-cmd = os.popen(r'crf_test -m ' + model + ' ' + test_data)
-result = cmd.read()
 
-print(result)
+def crf_test(model, test_data):
+    cmd = os.popen(r'crf_test -m ' + model + ' ' + test_data)
+    result = cmd.read()
+    return result
+
+def create_confusion_matrix(test):
+    tmp1 = test.splitlines()
+    for line in tmp1:
+        tmp2 = []
+        i = re.split(r'\t', line)
+        tmp2.append(i[len[i]-1])
+        tmp2.append(i[len[i]])
+    return tmp2
+
+
+if __name__ == '__main__':
+    test = crf_test(model, test_data)
+    print(create_confusion_matrix(test))
